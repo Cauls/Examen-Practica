9e8b88c5c36e1e7a1ddf3f5ae748232fa3c1a81e
Sol <alvaro.figueroa3206@gmail.com>
2025-10-01 19:34:43 +0200

    Fin del parcial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        ShoppingCart cart = new ShoppingCart();
+        cart.addProduct("manzana", 3);
+        cart.addProduct("leche", 2);
+        cart.addProduct("pan", 1);
+        cart.displayCart();
+        cart.updateCantidad("manzana", 5);
+        cart.eliminarProducto("pan");
+        cart.displayCart();
+        cart.addProduct("pescado", 3);
+        cart.addProduct("naranja", 10);
+        cart.displayCart();
     }
 }
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,18 +1,18 @@
 import java.util.HashMap;
 
 public class ShoppingCart {
-    private HashMap<String, Integer> cart;
-    private HashMap<String, Double> prices;
+    private final HashMap<String, Integer> cart;
+    private final HashMap<String, Double> prices;
 
-    public ShoppingCart(HashMap<String, Integer> cart, HashMap<String, Double> prices) {
-        this.cart = cart;
-        this.prices = prices;
+    public ShoppingCart() {
+        this.cart = new HashMap<>();
+        this.prices = new HashMap<>();
 
-        this . prices . put(" manzana ", 0.50) ;
-        this . prices . put(" platano ", 0.30) ;
-        this . prices . put(" naranja ", 0.60) ;
-        this . prices . put(" leche ", 2.50) ;
-        this . prices . put(" pan", 1.50) ;
+        this . prices . put("manzana", 0.50) ;
+        this . prices . put("platano", 0.30) ;
+        this . prices . put("naranja", 0.60) ;
+        this . prices . put("leche", 2.50) ;
+        this . prices . put("pan", 1.50) ;
     }
 
     public void addProduct(String prod, int cant){
@@ -20,13 +20,44 @@ public class ShoppingCart {
         if (prices.containsKey(prod)){
             if (cart.containsKey(prod)){
                 cart.put(prod, cart.get(prod) + cant);
+                System.out.println("Se han añadido " + cant + " unidades del producto " + prod + " (total:" + cart.get(prod)+ ")");
             }else{
                 cart.put(prod, cant);
+                System.out.println("Se han añadido " + cant + " unidades del producto " + prod);
             }
         }else{
             System.out.println("El producto " + prod + " no existe");
         }
     }
 
+    public void displayCart(){
+        double total = 0.00;
+        System.out.println("\nContenido del carrito:\n__________________");
+        cart.forEach((p,c) -> System.out.println(p + " - Cantidad: " + c + " - Precio: " + (prices.get(p) * c)));
+        for(String p : cart.keySet()){
+            total+=cart.get(p) * prices.get(p);
+        }
+        System.out.println("\nTotal: " + total);
+    }
 
+    public void updateCantidad(String producto, int cantidad){
+        producto = producto.toLowerCase();
+        if (cart.containsKey(producto)){
+            cart.put(producto, cantidad);
+            System.out.println("Actualizada cantidad de " + producto + " a " + cantidad);
+        }else{
+            System.out.println("No se ha encontrado el producto " + producto);
+        }
+
+    }
+
+    public void eliminarProducto(String producto){
+        producto = producto.toLowerCase();
+        if(cart.containsKey(producto)){
+            cart.remove(producto);
+            System.out.println("Se ha eliminado " + producto);
+        }else{
+            System.out.println("Producto " + producto + "no encontrado");
+        }
+    }
 }
